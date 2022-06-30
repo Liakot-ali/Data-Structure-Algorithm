b5e14c3f7c57af2a44ae1b6068cade23f07baf26
Liakot-ali <liakotali2k18@gmail.com>
2022-06-30 22:42:25 +0600

    delete from head and tail added

--- Doubly Linked List/doubly linked list.cpp
+++ Doubly Linked List/doubly linked list.cpp
@@ -16,6 +16,7 @@ public:
 node* head = NULL;
 node* tail = NULL;
 
+///----insert value in the last--------
 void InsertTail(int value){
     node* n = new node(value);
     if(head == NULL){   //when add value for first time
@@ -34,6 +35,8 @@ void InsertTail(int value){
     temp->next = n;
     n->pre = temp;*/
 }
+
+///-------insert value at first-------
 void InsertHead(int value){
     node* n = new node(value);
     if(head == NULL)
@@ -47,7 +50,41 @@ void InsertHead(int value){
     head = n;
 }
 
+void DeleteTail(){
+    if(tail == NULL){
+        cout<<"The list is empty"<<endl;
+        return;
+    }
+    if(tail->pre == NULL){
+        tail = NULL;
+        head = NULL;
+        return;
+    }
+    node* temp = tail->pre;
+    temp->next = tail->next;
+    tail = temp;
+}
+void DeleteHead(){
+    if(head == NULL){
+        cout<<"The list is empty"<<endl;
+        return;
+    }
+    if(head->next == NULL){
+        head = NULL;
+        tail = NULL;
+        return;
+    }
+    node* temp = head->next;
+    temp->pre = head->pre;
+    head = temp;
+}
+
 void displayForward(){
+    if(head == NULL){
+        cout<<"The list is empty"<<endl;
+        return;
+    }
+    cout<<"This is forward list:"<<endl;
     node* temp = head;
     while(temp->next != NULL){
         cout<<temp->data<<" ";
@@ -56,6 +93,11 @@ void displayForward(){
     cout<<temp->data<<endl;
 }
 void displayBackward(){
+    if(tail == NULL){
+        cout<<"The list is empty"<<endl;
+        return;
+    }
+    cout<<"This is backward list:"<<endl;
     node* tmp = tail;
     while(tmp->pre != NULL){
         cout<<tmp->data<<" ";
@@ -66,12 +108,73 @@ void displayBackward(){
 
 int main(){
     cout<<"This is doubly linked list program"<<endl;
-    InsertTail(5);
-    displayForward();
-    InsertTail(55);
-    InsertHead(15);
-    InsertTail(25);
-    displayBackward();
+    while(true)
+    {
+        int input;
+        printf("Input:\n\t 1.For adding data\n\t 2.For Display list\n\t 3.For delete a element\n\t"
+                " 4.For search a value\n\t 0.For exit\n");
+        cin>>input;
+        if(input == 1)
+        {
+            int value;
+            char ch;
+            cout<<"Enter the value:";
+            cin>>value;
+            cout<<"Insert at HEAD(h) or TAIL(t)?:";
+            cin>>ch;
+            take:
+            if(ch == 'h'|| ch == 'H'){
+                InsertHead(value);
+            }else if(ch == 't' || ch == 'T'){
+                InsertTail(value);
+            }else{
+                cout<<"Input H or T:";
+                cin>>ch;
+                goto take;
+            }
+        }
+        else if(input == 2)
+        {
+            displayForward();
+            displayBackward();
+        }
+        else if(input == 3)
+        {
+            displayForward();
+            char ch;
+            cout<<"Delete from HEAD(h) or TAIL(t)?:";
+            cin>>ch;
+            takeAgain:
+            if(ch == 'h'|| ch == 'H'){
+                DeleteHead();
+                displayForward();
+            }else if(ch == 't' || ch == 'T'){
+                DeleteTail();
+                displayForward();
+            }else{
+                cout<<"Input H or T:";
+                cin>>ch;
+                goto takeAgain;
+            }
+        }
+        else if(input == 4){
+            cout<<"Under construction"<<endl;
+            /*
+            int val;
+            cout<<"Which value you want to search? ";
+            cin>>val;
+            Search(val);
+            */
+        }
+        else if(input == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            cout<<"Invalid input"<<endl;
+        }
+    }
 
     return 0;
 }
