723b72c6a967a055f8b15498d6af84e39dc9c8e8
Liakot-ali <liakotali2k18@gmail.com>
2022-07-02 10:08:53 +0600

    circular linked list insertion and deletion added(head & tail)

--- Circular Linked List/singly circuler linked list.cpp
+++ Circular Linked List/singly circuler linked list.cpp
@@ -41,10 +41,55 @@ void insertHead(int val){
     head = n;
     temp->next = head;
 }
-void display(){
+void deleteTail(){
+    if(head == NULL){
+        cout<<"No item to delete"<<endl;
+        return;
+    }
+    node* temp = head;
+    if(temp->next == head){ //if there are only one element
+        head = NULL;
+        return;
+    }
+    while(temp->next != head){
+        node* nextNode = temp->next;
+        if(nextNode->next == head){
+            nextNode->next = NULL;  //break the chain
+            temp->next = head;
+            break;
+        }
+        temp = temp->next;
+    }
+    return;
+}
+void deleteHead(){
+    if(head == NULL){
+        cout<<"No item to delete"<<endl;
+        return;
+    }
+    node* tmp = head;
+
+    if(tmp->next == head){
+        head = NULL;
+        return;
+    }
+    node* temp = head;
+    while(temp->next != NULL){
+        if(temp->next == head){
+            break;
+        }
+        temp = temp->next;
+    }
+    head = tmp->next;
+    temp->next = head;
+    tmp->next = NULL;
+    return;
+
+}
+bool display(){
     if(head == NULL){
         cout<<"The list is empty"<<endl;
-        return;
+        return false;
     }
     node* temp = head;
     while(temp->next != head){
@@ -52,23 +97,75 @@ void display(){
         temp = temp->next;
     }
     cout<<temp->data<<endl;
+    return true;
 }
 int main(){
     cout<<"This is circular singly linked list program"<<endl;
-    display();
-    insertTail(15);
-    display();
-    insertTail(5);
-    display();
-    insertTail(154);
-    display();
-    insertHead(12);
-    display();
-    insertTail(145);
-    insertTail(165);
-    insertTail(115);
-    display();
-    insertHead(89);
-    display();
+    while(true)
+    {
+        int input;
+        printf("Input:\n\t 1.For adding data\n\t 2.For Display list\n\t 3.For delete a element\n\t"
+                " 4.For search a value\n\t 0.For exit\n");
+        cin>>input;
+        if(input == 1)
+        {
+            int value;
+            char ch;
+            cout<<"Enter the value:";
+            cin>>value;
+            cout<<"Insert at HEAD(h) or TAIL(t)?:";
+            cin>>ch;
+            take:
+            if(ch == 'h'|| ch == 'H'){
+                insertHead(value);
+            }else if(ch == 't' || ch == 'T'){
+                insertTail(value);
+            }else{
+                cout<<"Input H or T:";
+                cin>>ch;
+                goto take;
+            }
+        }
+        else if(input == 2)
+        {
+            display();
+        }
+        else if(input == 3)
+        {
+            if(display()){
+                char ch;
+                cout<<"Delete from HEAD(h) or TAIL(t)?:";
+                cin>>ch;
+                takeAgain:
+                if(ch == 'h'|| ch == 'H'){
+                    deleteHead();
+                    display();
+                }else if(ch == 't' || ch == 'T'){
+                    deleteTail();
+                    display();
+                }else{
+                    cout<<"Input H or T:";
+                    cin>>ch;
+                    goto takeAgain;
+                }
+            }
+        }
+        else if(input == 4){
+            cout<<"Under construction"<<endl;
+            /*int val;
+            cout<<"Which value you want to search? ";
+            cin>>val;
+            Search(val);
+            */
+        }
+        else if(input == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            cout<<"Invalid input"<<endl;
+        }
+    }
     return 0;
 }
