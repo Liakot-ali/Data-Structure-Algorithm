32d2a6731ac99ec62e2f0dca7f3a6a912842a7d0
Liakot-ali <liakotali2k18@gmail.com>
2022-07-19 22:22:27 +0600

    Changing input layout, adding extra message and remove extra line

--- Binary Search Tree/binary search tree.cpp
+++ Binary Search Tree/binary search tree.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class node{
+class node{  ///Node of the BST which has 2 pointer
 public:
     int data;
     node* left;
@@ -13,40 +13,73 @@ public:
         right = NULL;
     }
 };
-node* R = NULL;
+node* R = NULL; ///----Main root, which point the root node
 node* insertNode(node* root, int val){
     if(R == NULL){
         R = new node(val);
         root = new node(val);
-        //cout<<root->data<<endl;
         return root;
     }
     if(root == NULL){
         root = new node(val);
-        //cout<<root->data<<endl;
         return root;
     }
     if(val > root->data){
         root->right = insertNode(root->right, val);
-        //root->right = n;
     }else{
         root->left = insertNode(root->left, val);
-        //root->left = n;
     }
     return root;
 }
-void inorder(node* root){
+void displayInorder(node* root){ ///-------Traversing the BST in InOrder----
+
+    if(R == NULL){
+        cout<<"The BST is empty"<<endl;
+        return;
+    }
     if(root == NULL){
         return;
     }
-    inorder(root->left);
+    displayInorder(root->left);
     cout<<root->data<<" ";
-    inorder(root->right);
-    //cout<<root->data<<" ";
+    displayInorder(root->right);
 }
 int main(){
     cout<<"This is Binary Search Tree Program using Linked list"<<endl;
 
+    while(true)
+    {
+        int input;
+        printf("Input:\n\t 1.For insert data\n\t 2.For Show the BST in InOrder\n\t 3.For delete a node\n\t"
+                " 0.For exit\n");
+        cin>>input;
+        if(input == 1)
+        {
+            int value;
+            cout<<"Enter the value:";
+            cin>>value;
+            insertNode(R, value);
+        }
+        else if(input == 2)
+        {
+            displayInorder(R);
+            cout<<endl;
+        }
+        else if(input == 3)
+        {
+            cout<<"Under Construction"<<endl;
+        }
+        else if(input == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            cout<<"Invalid input"<<endl;
+        }
+    }
+
+    /*
     insertNode(R, 55);
     insertNode(R, 10);
     insertNode(R, 7);
@@ -56,5 +89,6 @@ int main(){
     insertNode(R, 4);
     insertNode(R, 70);
     inorder(R);
+    */
     return 0;
 }
