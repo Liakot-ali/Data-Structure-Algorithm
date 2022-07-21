c6fe0c9fb5b0f7695e1cff2d8a527e21bf7d82da
Liakot-ali <liakotali2k18@gmail.com>
2022-07-21 20:47:54 +0600

    Updating interface

--- Binary Search Tree/binary search tree.cpp
+++ Binary Search Tree/binary search tree.cpp
@@ -2,56 +2,94 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class node{  ///Node of the BST which has 2 pointer
+class node   ///Node of the BST which has 2 pointer
+{
 public:
     int data;
     node* left;
     node* right;
-    node(int val){
+    node(int val)
+    {
         data = val;
         left = NULL;
         right = NULL;
     }
 };
 node* R = NULL; ///----Main root, which point the root node
-node* insertNode(node* root, int val){
-    if(R == NULL){
+node* insertNode(node* root, int val)
+{
+    if(R == NULL)
+    {
         R = new node(val);
         root = new node(val);
         return root;
     }
-    if(root == NULL){
+    if(root == NULL)
+    {
         root = new node(val);
         return root;
     }
-    if(val > root->data){
+    if(val > root->data)
+    {
         root->right = insertNode(root->right, val);
-    }else{
+    }
+    else
+    {
         root->left = insertNode(root->left, val);
     }
     return root;
 }
-void displayInorder(node* root){ ///-------Traversing the BST in InOrder----
+void displayInorder(node* root)  ///-------Traversing the BST in InOrder----
+{
 
-    if(R == NULL){
+    if(R == NULL)
+    {
         cout<<"The BST is empty"<<endl;
         return;
     }
-    if(root == NULL){
+    if(root == NULL)
+    {
         return;
     }
     displayInorder(root->left);
     cout<<root->data<<" ";
     displayInorder(root->right);
 }
-int main(){
+bool Search(node* root, int val)
+{
+    if(R == NULL)
+    {
+        cout<<"The BST is empty"<<endl;
+        return false;
+    }
+    if(root == NULL)
+    {
+        cout<<"The value is not in the BST"<<endl;
+        return false;
+    }
+    if(val == root->data)
+    {
+        cout<<"The value is in the list"<<endl;
+        return true;
+    }
+    else if(val > root->data)
+    {
+        Search(root->right, val);
+    }
+    else
+    {
+        Search(root->left, val);
+    }
+}
+int main()
+{
     cout<<"This is Binary Search Tree Program using Linked list"<<endl;
 
     while(true)
     {
         int input;
-        printf("Input:\n\t 1.For insert data\n\t 2.For Show the BST in InOrder\n\t 3.For delete a node\n\t"
-                " 0.For exit\n");
+        printf("Input:\n\t 1.For insert data\n\t 2.For Show the BST in InOrder\n\t 3.For search a value\n\t"
+               " 0.For exit\n");
         cin>>input;
         if(input == 1)
         {
@@ -67,7 +105,17 @@ int main(){
         }
         else if(input == 3)
         {
-            cout<<"Under Construction"<<endl;
+            int value;
+            cout<<"Enter the value: ";
+            cin>>value;
+            if(Search(R, value))
+            {
+                cout<<"Got it"<<endl;
+            }
+            else
+            {
+                cout<<"Sorry"<<endl;
+            }
         }
         else if(input == 0)
         {
